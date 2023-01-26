f866054b1b3988dc24e6a131b6c536f17042ed05
Jacob Orshalick <jacob@solutionsfit.com>
2023-01-26 15:46:48 -0600

    fix link back to tutorial

--- src/components/FooterCopyright.tsx
+++ src/components/FooterCopyright.tsx
@@ -3,23 +3,6 @@ import { COPYRIGHT } from "../lib/constants";
 const FooterCopyright = () => (
   <div className="footer-copyright">
     © Copyright {new Date().getFullYear()} "{COPYRIGHT}".
-    {/*
-     * PLEASE READ THIS SECTION
-     * We'll really appreciate if you could have a link to our website
-     * The link doesn't need to appear on every pages, one link on one page is enough.
-     * Thank you for your support it'll mean a lot for us.
-     */}
-    <style jsx>
-      {`
-        .footer-copyright :global(a) {
-          @apply text-primary-500;
-        }
-
-        .footer-copyright :global(a:hover) {
-          @apply underline;
-        }
-      `}
-    </style>
   </div>
 );
 
--- src/components/PoweredBy.tsx
+++ src/components/PoweredBy.tsx
@@ -4,12 +4,18 @@ const PoweredBy = () => {
   return (
     <div className="bg-neutral-500 border-neutral-500 text-white">
       <Container>
-        <div className="py-2 text-center text-sm">
+        <div className="py-2 text-center text-sm mb-4">
+          {/*
+           * PLEASE READ THIS SECTION
+           * I would really appreciate it if you could keep the link to the tutorial
+           * on your site.  Not on every page, but on at least one page would really
+           * be appreciated.  Thank you for your help!
+           */}
           <>
             This site was created using{" "}
             <a
-              href={"https://jacoborshalick.me/posts/"}
-              className="underline hover:text-blue-600 duration-200 transition-colors"
+              href="https://jacoborshalick.me/posts/nextjs-and-aws-amplify-host-a-personal-blog-in-under-30-minutes"
+              className="underline hover:text-gray-300 duration-200 transition-colors"
             >
               this tutorial
             </a>
